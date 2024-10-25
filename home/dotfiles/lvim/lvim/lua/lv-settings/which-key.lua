df4dd458d3044dbb9b4e7a8fb87b7ff4f4f223b6
yanosea <myanoshi0626@gmail.com>
2024-10-25 13:37:46 +0900

    ✨feat(lunarvim): enhance which-key and CopilotChat mappings
    - added new mappings for `CopilotChat` features
      like `explain` and `optimize`
    - reorganized `which-key` mappings for better readability
    - updated `CopilotChat` window configuration
      for improved user experience

--- home/dotfiles/lvim/lvim/lua/lv-settings/which-key.lua
+++ home/dotfiles/lvim/lvim/lua/lv-settings/which-key.lua
@@ -2,49 +2,45 @@
 -- chatgpt
 lvim.builtin.which_key.mappings["a"] = {
   name = "+Chatgpt",
-  -- GpChatNew tabnew
-  t = { "<CMD>GpChatNew tabnew<CR>", "GpChatNew tabnew" },
-  -- GpChatNew split
-  s = { "<CMD>GpChatNew split<<CR>", "GpChatNew split" },
-  -- GpChatNew vsplit
-  v = { "<CMD>GpChatNew vsplit<CR>", "GpChatNew vsplit" },
-  -- GpChatNew popup
-  p = { "<CMD>GpChatNew popup<CR>", "GpChatNew popup" },
   -- GpChatFinder
   e = { "<CMD>GpChatFinder<CR>", "GpChatFinder" },
+  -- GpChatNew popup
+  p = { "<CMD>GpChatNew popup<CR>", "GpChatNew popup" },
+  -- GpChatNew split
+  s = { "<CMD>GpChatNew split<<CR>", "GpChatNew split" },
+  -- GpChatNew tabnew
+  t = { "<CMD>GpChatNew tabnew<CR>", "GpChatNew tabnew" },
+  -- GpChatNew vsplit
+  v = { "<CMD>GpChatNew vsplit<CR>", "GpChatNew vsplit" },
 }
 -- copilot chat
 lvim.builtin.which_key.mappings["C"] = {
   name = "+CopilotChat",
-  -- CopilotChat
-  t = { "<CMD>CopilotChat<CR>", "CopilotChat chat" },
-  -- CopilotChatStop
-  c = { "<CMD>CopilotChatStop<CR>", "CopilotChat stop" },
-  -- CopilotChatSave
-  s = { "<CMD>CopilotChatSave<CR>", "CopilotChat save" },
-  -- CopilotChatLoad
-  l = { "<CMD>CopilotChatLoad<CR>", "CopilotChat load" },
+  -- CopilotChatCommitStaged
+  c = { "<CMD>CopilotChatCommitStaged<CR>", "CopilotChat commit staged" },
   -- CopilotChatDocs
   d = { "<CMD>CopilotChatDocs<CR>", "CopilotChat docs" },
+  -- CopilotChatExplain
+  e = { "<CMD>CopilotChatExplain<CR>", "CopilotChat explain" },
   -- CopilotChatFix
   f = { "<CMD>CopilotChatFix<CR>", "CopilotChat fix" },
-  -- CopilotChatReview
-  r = { "<CMD>CopilotChatReview<CR>", "CopilotChat review" },
+  -- CopilotChatLoad
+  l = { "<CMD>CopilotChatLoad<CR>", "CopilotChat load" },
+  -- CopilotChatOptimize
+  o = { "<CMD>lua CopilotChatOptimize()<CR>", "CopilotChat optimize" },
   -- CopilotChatBuffer
   q = { "<CMD>lua CopilotChatBuffer()<CR>", "CopilotChat quick chat" },
+  -- CopilotChatReview
+  r = { "<CMD>CopilotChatReview<CR>", "CopilotChat review" },
+  -- CopilotChatSave
+  s = { "<CMD>CopilotChatSave<CR>", "CopilotChat save" },
+  -- CopilotChat
+  t = { "<CMD>CopilotChatToggle<CR>", "CopilotChat chat toggle" },
+  -- CopilotChatTests
+  T = { "<CMD>CopilotChatTests<CR>", "CopilotChat tests" },
 }
--- toggleterm
-lvim.builtin.which_key.mappings["t"] = { "<CMD>ToggleTerm<CR>", "ToggleTerm" }
 -- fuzzy motion
 lvim.builtin.which_key.mappings["<SPACE>"] = { "<CMD>FuzzyMotion<CR>", "FuzzyMotion" }
--- yankring
-lvim.builtin.which_key.mappings["y"] = {
-  name = "+Yankring",
-  -- YRShow
-  h = { "<CMD>YRShow<CR>", "YankRing show" },
-}
--- outline
-lvim.builtin.which_key.mappings["o"] = { "<CMD>Outline<CR>", "Outline" }
 -- lsp
 table.insert(lvim.builtin.which_key.mappings["l"], {
   -- todo-comments
@@ -54,3 +50,13 @@ table.insert(lvim.builtin.which_key.mappings["l"], {
   -- Trouble diagnostics toggle
   T = { "<CMD>Trouble diagnostics toggle<CR>", "TroubleDiagnostics" },
 })
+-- outline
+lvim.builtin.which_key.mappings["o"] = { "<CMD>Outline<CR>", "Outline" }
+-- toggleterm
+lvim.builtin.which_key.mappings["t"] = { "<CMD>ToggleTerm<CR>", "ToggleTerm" }
+-- yankring
+lvim.builtin.which_key.mappings["y"] = {
+  name = "+Yankring",
+  -- YRShow
+  h = { "<CMD>YRShow<CR>", "YankRing show" },
+}
