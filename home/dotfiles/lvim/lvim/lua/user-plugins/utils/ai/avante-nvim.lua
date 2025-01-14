133cfac5d2052b9b25cbae6b69640588786cd8be
yanosea <myanoshi0626@gmail.com>
2025-01-14 19:56:41 +0900

    ✨feat(lunarvim): add `avante.nvim` plugin and reorganize AI keymap
    - add `avante.nvim` plugin integration with Copilot
    - add new `which-key` mappings for `Avante` commands
      under `<Leader>a`
    - move existing `ChatGPT` commands to `<Leader>A`
      with uppercase keys
    - update key bindings in `gp-nvim.lua` to match new uppercase scheme
    - configure `avante.nvim` with right-side window and minimized diff view

---
+++ home/dotfiles/lvim/lvim/lua/user-plugins/utils/ai/avante-nvim.lua
@@ -0,0 +1,47 @@
+-- github copilot assistant plugin for avante.nvim
+table.insert(lvim.plugins, {
+  "yetone/avante.nvim",
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-tree/nvim-web-devicons",
+    "zbirenbaum/copilot.lua",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+  event = "VeryLazy",
+  lazy = false,
+  build = "make",
+  version = false,
+  opts = {
+    provider = "copilot",
+    auto_suggestions_provider = "copilot",
+    behaviour = {
+      auto_suggestions = false,
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = false,
+      minimize_diff = true,
+    },
+    windows = {
+      position = "right",
+      wrap = true,
+      width = 30,
+    },
+  },
+})
