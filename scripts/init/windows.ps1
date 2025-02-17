2c4e3e78c8ba7a385ba3519827849d7f157b1202
yanosea <myanoshi0626@gmail.com>
2025-02-17 10:35:51 +0900

    ✨feat(win): add software installation to Windows init script
    - add pwsh 7 installation via winget
    - add git installation via winget
    - add ghq installation via scoop
    - add informative messages for each installation step

--- scripts/init/windows.ps1
+++ scripts/init/windows.ps1
@@ -1,7 +1,17 @@
 # yanosea windows initialize script
+# install pwsh 7
+Write-Host "`ninstall winget!" -ForegroundColor Yellow
+winget install Microsoft.PowerShell
+# install git
+Write-Host "`ninstall git!" -ForegroundColor Yellow
+winget install git
 # install scoop
+Write-Host "`ninstall scoop!" -ForegroundColor Yellow
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+# install ghq via scoop
+Write-Host "`ninstall ghq!" -ForegroundColor Yellow
+scoop install ghq
 # clone ghq repose
 Write-Host "`nclone ghq repos!" -ForegroundColor Yellow
 $filePath = "$HOME\ghq\github.com\yanosea\yanoNixFiles\pkglist\ghq\pkglist.txt"
