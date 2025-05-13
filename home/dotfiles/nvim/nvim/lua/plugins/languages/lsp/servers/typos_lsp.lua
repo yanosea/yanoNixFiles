7b83f042e0eca92543366d0df36c2f6ee11120c2
yanosea <myanoshi0626@gmail.com>
2025-05-13 19:11:12 +0900

    wip

---
+++ home/dotfiles/nvim/nvim/lua/plugins/languages/lsp/servers/typos_lsp.lua
@@ -0,0 +1,10 @@
+-- typos lsp config
+local M = {}
+function M.setup()
+  vim.lsp.config("*", {
+    init_options = {
+      config = vim.fn.stdpath("config") .. "/lua/plugins/languages/lsp/servers/config/typos.toml",
+    },
+  })
+end
+return M
