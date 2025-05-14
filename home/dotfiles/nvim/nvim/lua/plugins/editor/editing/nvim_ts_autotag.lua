e597f683b9088fac4654e5956728a22cc6544349
yanosea <myanoshi0626@gmail.com>
2025-05-14 23:14:14 +0900

    wip

--- home/dotfiles/nvim/nvim/lua/plugins/editor/editing/nvim_ts_autotag.lua
+++ home/dotfiles/nvim/nvim/lua/plugins/editor/editing/nvim_ts_autotag.lua
@@ -4,5 +4,14 @@ return {
     "windwp/nvim-ts-autotag",
     lazy = true,
     ft = { "html", "xml", "jsx", "tsx", "vue", "svelte", "php", "rescript", "astro" },
+    config = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = true,
+        },
+      })
+    end,
   },
 }
