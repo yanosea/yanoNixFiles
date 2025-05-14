a10ee08502655015d39944f13e8e492959181ced
yanosea <myanoshi0626@gmail.com>
2025-05-14 23:08:17 +0900

    wip

--- home/dotfiles/nvim/nvim/lua/plugins/editor/editing/nvim_autopairs.lua
+++ home/dotfiles/nvim/nvim/lua/plugins/editor/editing/nvim_autopairs.lua
@@ -5,41 +5,6 @@ return {
     lazy = true,
     event = "InsertEnter",
     dependencies = { "nvim-treesitter/nvim-treesitter", "hrsh7th/nvim-cmp" },
-    config = function()
-      local autopairs = require("nvim-autopairs")
-      autopairs.setup({
-        check_ts = true,
-        enable_check_bracket_line = false,
-        ts_config = {
-          lua = { "string", "source" },
-          javascript = { "string", "template_string" },
-          java = false,
-        },
-        disable_filetype = { "TelescopePrompt", "spectre_panel" },
-        disable_in_macro = false,
-        disable_in_visualblock = false,
-        disable_in_replace_mode = true,
-        ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
-        enable_moveright = true,
-        enable_afterquote = true,
-        map_c_w = false,
-        map_bs = true,
-        fast_wrap = {
-          map = "<M-e>",
-          chars = { "{", "[", "(", '"', "'" },
-          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-          offset = 0, -- Offset from pattern match
-          end_key = "$",
-          keys = "qwertyuiopzxcvbnmasdfghjkl",
-          check_comma = true,
-          highlight = "Search",
-          highlight_grey = "Comment",
-        },
-      })
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp = require("cmp")
-      cmp.event:off("confirm_done", cmp_autopairs.on_confirm_done())
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
+    config = true,
   },
 }
