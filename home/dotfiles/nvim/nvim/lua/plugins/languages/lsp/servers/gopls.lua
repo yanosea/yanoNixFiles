ba05c57055454b569d33542a1ee3f47130dbd762
yanosea <myanoshi0626@gmail.com>
2025-05-15 22:53:11 +0900

    wip

--- home/dotfiles/nvim/nvim/lua/plugins/languages/lsp/servers/gopls.lua
+++ home/dotfiles/nvim/nvim/lua/plugins/languages/lsp/servers/gopls.lua
@@ -9,12 +9,19 @@ function M.setup()
       },
       staticcheck = true,
       hints = {
+        -- show variable types in assignment statements
         assignVariableTypes = true,
+        -- display inferred types for struct literal fields
         compositeLiteralFields = true,
+        -- show types for composite literals
         compositeLiteralTypes = true,
+        -- show values for constants
         constantValues = true,
+        -- display types of function parameters
         functionTypeParameters = true,
+        -- show parameter names in function calls
         parameterNames = true,
+        -- show types for variables in range statements
         rangeVariableTypes = true,
       },
     },
--- home/dotfiles/nvim/nvim/lua/plugins/languages/lsp/servers/typos_lsp.lua
+++ home/dotfiles/nvim/nvim/lua/plugins/languages/lsp/servers/typos_lsp.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.setup()
   vim.lsp.config("*", {
     init_options = {
+      -- set config file path
       config = vim.fn.stdpath("config") .. "/lua/plugins/languages/lsp/servers/config/typos.toml",
       diagnosticSeverity = "hint",
     },
