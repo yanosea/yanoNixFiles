0706ab80a201d3a11d27ed370485647b1974f9ff
yanosea <81510859+yanosea@users.noreply.github.com>
2025-06-15 17:23:02 +0900

    ✨feat: add `fcitx5` with SKK input method for Japanese
    - add `fcitx5` configuration files with `Nord-Dark` theme
    - configure SKK input method for Japanese typing
    - setup autostart and environment variables in `Hyprland`
    - add `fcitx5` and required addons (`mozc`, `skk`, `nord`) to Nix modules
    - disable old IME toggle hotkey

--- modules/core/i18n.nix
+++ modules/core/i18n.nix
@@ -1,6 +1,20 @@
+{ pkgs, ... }:
 {
   # i18n
   i18n = {
     defaultLocale = "en_US.UTF-8";
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        addons = with pkgs; [
+          fcitx5-gtk
+          fcitx5-mozc
+          fcitx5-nord
+          fcitx5-skk
+        ];
+        waylandFrontend = true;
+      };
+    };
   };
 }
