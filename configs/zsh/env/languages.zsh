3f12e0a17a07964216464c3f4d31706fbc024562
yanosea <81510859+yanosea@users.noreply.github.com>
2025-08-11 02:11:28 +0900

    ✨feat(zsh): add deno binary path to PATH
    - add `deno` binary path to `PATH` environment variable
    - reorganize path exports by moving deno before go

--- configs/zsh/env/languages.zsh
+++ configs/zsh/env/languages.zsh
@@ -2,6 +2,8 @@
 # programming languages environment settings
 #
 
+# deno
+export PATH=$PATH:$HOME/.deno/bin
 # go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
@@ -12,4 +14,3 @@ export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PATH:$PYENV_ROOT/bin
 # rust/cargo
 export PATH=$PATH:$HOME/.cargo/bin
-
