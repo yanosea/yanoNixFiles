358938c0c05fc0f0a87be7fd0576afa0a50f67f7
yanosea <81510859+yanosea@users.noreply.github.com>
2025-12-23 09:45:03 +0900

    🧹chore(mcp): remove `context7` mcp installer scripts
    - delete `installContext7Mcp.sh` script
    - delete `uninstallContext7Mcp.sh` script
    - remove symlink configurations for `context7` scripts from
    `symlinks.nix`
    - `context7` is now available via claude code `/plugin` command

--- outputs/home-manager/shared-modules/cli/symlinks.nix
+++ outputs/home-manager/shared-modules/cli/symlinks.nix
@@ -9,11 +9,9 @@
         $DRY_RUN_CMD mkdir -p $HOME/.local/bin
         # create symbolic links to scripts
         $DRY_RUN_CMD ln -sf $HOME/ghq/github.com/yanosea/yanoNixFiles/ops/scripts/common/installChromeDevtoolsMcp.sh $HOME/.local/bin/installChromeDevtoolsMcp
-        $DRY_RUN_CMD ln -sf $HOME/ghq/github.com/yanosea/yanoNixFiles/ops/scripts/common/installContext7Mcp.sh $HOME/.local/bin/installContext7Mcp
         $DRY_RUN_CMD ln -sf $HOME/ghq/github.com/yanosea/yanoNixFiles/ops/scripts/common/installGitEmojiPrefixTemplate.sh $HOME/.local/bin/installGitEmojiPrefixTemplate
         $DRY_RUN_CMD ln -sf $HOME/ghq/github.com/yanosea/yanoNixFiles/ops/scripts/common/installNixFmtPreCommitHook.sh $HOME/.local/bin/installNixFmtPreCommitHook
         $DRY_RUN_CMD ln -sf $HOME/ghq/github.com/yanosea/yanoNixFiles/ops/scripts/common/uninstallChromeDevtoolsMcp.sh $HOME/.local/bin/uninstallChromeDevtoolsMcp
-        $DRY_RUN_CMD ln -sf $HOME/ghq/github.com/yanosea/yanoNixFiles/ops/scripts/common/uninstallContext7Mcp.sh $HOME/.local/bin/uninstallContext7Mcp
         # create vim configuration symlink
         $DRY_RUN_CMD ln -sf $XDG_CONFIG_HOME/vim $HOME/.vim
       '';
